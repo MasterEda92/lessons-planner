7ce965cb018893d936d6d08b6c8f16ffed45fa47
Stefan Eder <darkmaster.eder@gmail.com>
2023-10-23 21:51:57 +0200

    changed style of odd table entries to a contrast color

--- frontend/src/lib/pages/Subjects.svelte
+++ frontend/src/lib/pages/Subjects.svelte
@@ -102,15 +102,15 @@
               </tr>
           </thead>
           <tbody>
-              {#each subjects as subject (subject.Id)}
+              {#each subjects as subject, i (subject.Id)}
                   <tr class="bg-white hover:bg-gray-50">
-                      <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                      <th scope="row" class="{(i%2==0) ? "bg-white" : "bg-gray-100"} px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                           {subject.Name}
                       </th>
-                      <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                      <td class="{(i%2==0) ? "bg-white" : "bg-gray-100"} px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                           {subject.Notes}
                       </td>
-                      <td class="px-6 py-4 text-right">
+                      <td class="{(i%2==0) ? "bg-white" : "bg-gray-100"} px-6 py-4 text-right">
                           <button 
                               on:click={() => {
                                   const url = "/subjects/" + subject.Id;
@@ -119,7 +119,7 @@
                               <svg class="fill-purple-950" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M13.94 5 19 10.06 9.062 20a2.25 2.25 0 0 1-.999.58l-5.116 1.395a.75.75 0 0 1-.92-.921l1.395-5.116a2.25 2.25 0 0 1 .58-.999L13.938 5Zm7.09-2.03a3.578 3.578 0 0 1 0 5.06l-.97.97L15 3.94l.97-.97a3.578 3.578 0 0 1 5.06 0Z"/></svg>
                           </button>
                       </td>
-                      <td class="px-6 py-4 text-right">
+                      <td class="{(i%2==0) ? "bg-white" : "bg-gray-100"} px-6 py-4 text-right">
                           <button on:click={ () => deleteSubject(subject.Id)}>
                               <svg class="fill-purple-950" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M21.5 6a1 1 0 0 1-.883.993L20.5 7h-.845l-1.231 12.52A2.75 2.75 0 0 1 15.687 22H8.313a2.75 2.75 0 0 1-2.737-2.48L4.345 7H3.5a1 1 0 0 1 0-2h5a3.5 3.5 0 1 1 7 0h5a1 1 0 0 1 1 1Zm-7.25 3.25a.75.75 0 0 0-.743.648L13.5 10v7l.007.102a.75.75 0 0 0 1.486 0L15 17v-7l-.007-.102a.75.75 0 0 0-.743-.648Zm-4.5 0a.75.75 0 0 0-.743.648L9 10v7l.007.102a.75.75 0 0 0 1.486 0L10.5 17v-7l-.007-.102a.75.75 0 0 0-.743-.648ZM12 3.5A1.5 1.5 0 0 0 10.5 5h3A1.5 1.5 0 0 0 12 3.5Z"/></svg>
                           </button>
