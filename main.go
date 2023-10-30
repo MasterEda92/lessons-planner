a1270945370aa424ba13828e474908176a19f5de
Stefan Eder <darkmaster.eder@gmail.com>
2023-10-30 23:03:35 +0100

    changed title of main window

--- main.go
+++ main.go
@@ -93,7 +93,7 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "myproject",
+		Title:  "Lessons-Planner",
 		Width:  1024,
 		Height: 768,
 		AssetServer: &assetserver.Options{
